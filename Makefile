1a1dc82d23794e17300e491e8af98c6d471c79d0
nniikon <kolya.kasparov@gmail.com>
2024-09-11 01:19:35 +0300

    add Makefile

---
+++ Makefile
@@ -0,0 +1,8 @@
+include config.mk
+
+all:
+	@$(MAKE) -C ./source
+	$(GXX) $(BUILD_DIR)/*.o -o $(BUILD_DIR)/$(EXEC_NAME) $(CFLAGS)
+
+clean:
+	@rm -rf $(BUILD_DIR)
---
+++ config.mk
@@ -0,0 +1,29 @@
+CFLAGS = -O0 -Wall \
+-Wmissing-declarations -Wcast-align -Wcast-qual \
+-Wchar-subscripts -Wconversion \
+-Wempty-body -Wfloat-equal -Wformat-nonliteral -Wformat-security \
+-Wformat=2 \
+-Wpacked -Wpointer-arith -Winit-self \
+-Wredundant-decls -Wshadow \
+-Wstrict-overflow=2 \
+-Wswitch-default -Wswitch-enum -Wundef -Wunreachable-code \
+-Wunused -Wvariadic-macros -Wno-nonnull \
+-Wno-missing-field-initializers -Wno-narrowing \
+-Wno-varargs -Wstack-protector -fcheck-new \
+-fstack-protector -fstrict-overflow \
+-fno-omit-frame-pointer \
+-fsanitize=address,bool,bounds,enum,float-cast-overflow,$\
+float-divide-by-zero,integer-divide-by-zero,leak,nonnull-attribute,null,$\
+object-size,return,returns-nonnull-attribute,shift,signed-integer-overflow,$\
+undefined,unreachable,vla-bound,vptr
+
+CFLAGS += -DDEBUG
+CFLAGS += -I../include/
+CFLAGS += -lsfml-graphics -lsfml-window -lsfml-system
+
+export CFLAGS
+
+export BUILD_DIR = ${CURDIR}/build
+export EXEC_NAME = CatmullRom 
+
+export GXX = g++
